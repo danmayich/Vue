52588afb8e7716f58a41220093a13e050bae4874
Dan <danielmayich@yahoo.com>
2025-08-01 16:21:09 -0400

    fixing counter app

---
+++ CounterApp/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import {ref} from "vue"
+
+  const count = ref(0);
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>
+      The current count is ...
+      </h4>
+      <h1>{{ count }}</h1>
+      <Button @click="count--">-</Button>
+      <Button @click="count++">+</Button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: blue;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  } 
+
+  div {
+    text-align: center;
+  }
+</style>
